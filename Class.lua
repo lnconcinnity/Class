8f6813d3e89d7b855a7354c4de4df3e299170c03
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2023-07-04 22:38:02 +0800

    Add files

---
+++ Class.lua
@@ -0,0 +1,227 @@
+local PRIVATE_MARKER = newproxy() -- only the class and inherited class can access it
+local PROTECTED_MARKER = newproxy() -- only the class and inherited can read and write; will only be read-only for other sources
+local INHERITED_MARKER = newproxy()
+local INHERITS_MARKER = newproxy()
+local STRICTIFIY_VALUE_MARKER = newproxy()
+local INTERNAL_MARKER = newproxy()
+local LOCKED_MARKER = newproxy() -- cant change after runtime fr
+
+local EXPLICIT_PRIVATE_PREFIX = "_"
+local EXPLICIT_PROTECTED_PREFIX = "__"
+
+local READ_PRIVATE_NO_ACCESS = "Attempted to read private \"%s\""
+local READ_INTERNAL_FAILED = "Cannot read internal property \"%s\""
+local WRITE_PRIVATE_NO_ACCESS = "Attempted to write private \"%s\" with the value \"%s\""
+local WRITE_PROTECTED_NO_ACCESS = "Attempted to write protected \"%s\" with the value \"%s\""
+local WRITE_INTERNAL_FAILED = "Cannot write internal property \"%s\""
+local CANNOT_WRITE_CONSTANT = "Attempted to overwrite constant \"%s\""
+local CANNOT_WRITE_LOCKED = "Attempt to overwrite locked property \"%s\""
+
+local EMPTY_STRING = ''
+
+local function hasFunction(class, method)
+	for _, fn in pairs(class) do
+		if type(fn) == "function" and fn == method then
+			return true
+		end
+	end
+	return false
+end
+
+local function canAccessViaInheritance(class, method)
+	for inherited in pairs(class[INHERITED_MARKER]) do
+		if inherited[INHERITS_MARKER][class] ~= nil and hasFunction(inherited, method) then
+			return true
+		end
+	end
+	return false
+end
+
+local function isWithinClassScope(class, includeInherited)
+	local level = 1--skip  __index
+	local within = false
+	local calledWithinFunction = false
+	while true do
+		level += 1
+		local method = debug.info(level, 'f')
+		local _, fnName = pcall(debug.info, method, 'n') -- anonymous and c-functions will return nil and prolly error?
+		if fnName == EMPTY_STRING then
+			break
+		end
+		
+		if hasFunction(class, method) then
+			calledWithinFunction = true
+		end
+
+		local result = (class[fnName] == method or calledWithinFunction) or (if includeInherited then canAccessViaInheritance(class, method) else false)
+		if result then
+			within = true
+			break
+		end
+	end
+	return within
+end
+
+local function isSpecialKey(key)
+	return type(key) == "userdata"
+end
+
+local function isAConstant(str: string)
+	if isSpecialKey(str) then return false end
+	str = str:gsub('_', '') -- remove underscores
+	local compare = str:gsub('(%l)', '')
+	return #compare == #str
+end
+
+local function isAccessingInternal(key)
+	return not isSpecialKey(key) and key:sub(1, 2) == EXPLICIT_PROTECTED_PREFIX and key:sub(#key - #EXPLICIT_PROTECTED_PREFIX+1) == EXPLICIT_PROTECTED_PREFIX
+end
+
+local function isAccessingProtected(key)
+	return not isSpecialKey(key) and key:sub(1, 2) == EXPLICIT_PROTECTED_PREFIX and key:sub(#key - #EXPLICIT_PROTECTED_PREFIX) ~= EXPLICIT_PROTECTED_PREFIX
+end
+
+local function isAccessingPrivate(key)
+	return not isSpecialKey(key) and key:sub(1, 2) ~= EXPLICIT_PROTECTED_PREFIX and key:sub(#key - #EXPLICIT_PROTECTED_PREFIX) ~= EXPLICIT_PROTECTED_PREFIX and key:sub(1, 1) == EXPLICIT_PRIVATE_PREFIX
+end
+
+local function initSelf(defaultProps)
+	local markers, realProps = {
+		PRIVATE_MARKER = {},
+		PROTECTED_MARKER = {},
+		LOCKED_MARKER = {},
+		INTERNAL_MARKER = {},
+		STRICTIFIY_VALUE_MARKER = {},
+	}, {}
+	
+	if defaultProps then
+		for key, value in pairs(defaultProps) do
+			if isSpecialKey(key) then
+				markers[key] = value
+			else
+				realProps[key] = value
+			end
+		end
+	end
+	
+	return markers, realProps
+end
+
+local function pasteSelf(self, props)
+	for key, value in pairs(props) do
+		self[key] = value
+	end
+end
+
+local function Class(defaultProps: {}?)
+	local meta = {}
+	local class = {}
+	class[INHERITS_MARKER] = {}
+	class[INHERITED_MARKER] = {}
+
+	function meta:__index(key)
+		local canAccessPrivate, canAccessInternal = isWithinClassScope(class, true), isWithinClassScope(class, false)
+		if isAccessingInternal(key) and not canAccessInternal then
+			error(string.format(READ_INTERNAL_FAILED, key), 2)
+		elseif isAccessingPrivate(key) and not canAccessPrivate then
+			error(string.format(READ_PRIVATE_NO_ACCESS, key), 2)
+		end
+
+		local public = rawget(self, key)
+		local protected = rawget(self, PROTECTED_MARKER)
+		if canAccessPrivate or canAccessInternal then
+			return rawget(self, INTERNAL_MARKER)[key] or rawget(self, PRIVATE_MARKER)[key] or protected[key] or public or class[key]
+		end
+		return protected[key] or public or class[key]
+	end
+
+	function meta:__newindex(key, value)
+		local accessingPrivate, accessingProtected, accessingLocked, accessingInternal = isAccessingPrivate(key), isAccessingProtected(key), rawget(self, LOCKED_MARKER)[key], isAccessingInternal(key)
+		if accessingLocked then
+			error(string.format(
+				if isAConstant(key) then CANNOT_WRITE_CONSTANT else CANNOT_WRITE_LOCKED,
+				key), 2)
+		elseif accessingInternal and not isWithinClassScope(class, false) then
+			error(string.format(WRITE_INTERNAL_FAILED, key), 3)
+		elseif (accessingPrivate or accessingProtected) and not isWithinClassScope(class, true) then
+			error(string.format(
+				if accessingPrivate then WRITE_PRIVATE_NO_ACCESS else WRITE_PROTECTED_NO_ACCESS,
+				tostring(key), tostring(value)), 2)
+		else
+			local predicate = rawget(self, STRICTIFIY_VALUE_MARKER)[key]
+			if predicate ~= nil then
+				local ok, err = predicate(value)
+				if not ok then
+					error(err or "Failed to set strict property", 2)
+				end
+			end
+			
+			if isAConstant(key) and not rawget(self, INTERNAL_MARKER).__canMakeConstants__ then
+				error("Cannot initialize constant '" .. key .. "', consider using class:__init() for this")
+			end
+
+			if accessingPrivate then
+				rawget(self, PRIVATE_MARKER)[key] = value
+			elseif accessingProtected then
+				rawget(self, PROTECTED_MARKER)[key] = value
+			else
+				if key == PROTECTED_MARKER or key == PRIVATE_MARKER then
+					error("Cannot override internal properties", 3)
+				end
+				rawset(self, key, value)
+			end
+		end
+	end
+
+	function class.new(...)
+		local markers, realProps = initSelf(defaultProps)
+		local self = setmetatable(markers, meta)
+		pasteSelf(self, realProps)
+		
+		self.__canMakeConstants__ = true
+		
+		if self.__init then
+			self:__init(...)
+		end
+
+		-- now check the constants
+		for key, value in pairs(self) do
+			if isAConstant(key) and type(value) ~= "function" then
+				self[LOCKED_MARKER][key] = true
+			end
+		end
+		self.__canMakeConstants__ = false
+		self:__lockProperty("__canMakeConstants__")
+
+		return self
+	end
+
+	function class.inherits(otherClass)
+		assert(otherClass[INHERITED_MARKER] ~= nil, "Cannot inherit from an unrelated class or table")
+		class[INHERITS_MARKER][otherClass] = true
+		otherClass[INHERITED_MARKER][class] = true
+	end
+
+	function class.extend()
+		local subClass = Class()
+		setmetatable(subClass, {__index = class})
+		return subClass
+	end
+
+	function class:__strictifyProperty(propName: string, predicate: (value: any) -> boolean)
+		self[STRICTIFIY_VALUE_MARKER][propName] = predicate
+	end
+
+	function class:__lockProperty(propName: string)
+		self[LOCKED_MARKER][propName] = true
+	end
+
+	function class:__unlockProperty(propName: string)
+		assert(not isAConstant(propName), "Cannot unlock a constant!")
+		self[LOCKED_MARKER][propName] = true
+	end
+
+	return class
+end
+
+return Class
---
+++ README.md
@@ -0,0 +1,59 @@
+<div align="center">
+<h1><b>Class</b></h1>
+<sup><b>Supercharge your classes</b></sup><br>
+<sup><a href="https://create.roblox.com/marketplace/asset/13953067746" target="_blank">Model</a></sup>
+<sup><a href="https://devforum.roblox.com/t/class/2446100/" target="_blank">DevForum</a></sup>
+</div>
+
+<div align="center"> <h4> Preface </h4> </div> <hr>
+
+Because of how bored I am lately and taking a short break to release my own game, I've decided, "You know what, what if I challenge myself to put in private and protected variables in LuaU classes?", and I did.
+
+With a bit of playing around with `debug.info`, I managed to make Class, the module being presented today!
+
+<div align="center"> <h4> What is Class? </h4> </div> <hr>
+
+Class is a module that allows you to make your classes go super.
+
+By how super you may ask? Class takes advantage of `debug.info`; by using the said method, we can slowly climb up from function to function until we get to a specific function that is under the class. With this, we are able to determine our current "level" within the code, thus, we can finally evaluate and interpret internal, private and protected properties!
+
+<div align="center"> <h4> Okay so, what can Class do? </h4> </div> <hr>
+
+Class is basically a generic class but one major alteration from the vanilla ones: we are able to make special properties. On top of that, we can initialize everything by creating `__init` method. It's best practiced to initialize everything under `class:__init()` than using the optional `defaultProps` parameter when creating a new class, as it's (supposedly) less prone to errors and other gimmicks!
+
+The table shown below will showcase you the special properties, their prefixes and descriptions:
+| Property type | Prefix | Example | Description |
+| --- | --- | --- | --- |
+| Constants | PROP_NAME | `self.MESSAGE` | Makes the property a constant; cannot be changed after it has been initialized under the `class:__init()` method or via the `defaultProps` option
+| Internal properties | \_\_propname__ | `self.__message__` | Makes the property internal; only the source class can access it |
+| Private properties | _ | `self._message` | Makes the property private; only the source class and inherited classes can access it |
+| Protected properties | __ | `self.__message` | Makes the property protected; other sources outside the source class or inherited classes can read the property but cannot overwrite the said property; the source class and inherited classes can change the property |
+
+<sup>Note, when userdatas; such as CFrames, Vector3s, Instances and such, are given as keys, they won't be assigned from any of these special property cases</sup>
+
+Class also gives you the option to lock the property; preventing the property in detail from being changed. To lock a property, you have to call `class:__lockProperty(propName)` and to unlock it you have to call `class:__unlockProperty(propName)` instead. <sub>Note, Constants are basically locked properties,  but you cannot unlock them; doing so will rase an error.</sub>
+
+Another addition of Class is to strictify properties, for example, if we want property `X` strictly only to be numbers, we have to do some checks and stuff; Class will introduce the `class:__strictifyProperty(propName, predicate)` method, where `predicate` is a function that returns a `boolean` value to validate the set process.
+
+<div align="center"> <h4> API and Examples </h4> </div> <hr>
+
+[details="API"]
+- ## `Class(defaultProps: {}?)`
+  - Creates a new `Class`
+***
+- ## `class.new(...any?): Class`
+  - Constructs a `Class` object; any properties within the `defaultProps` option containing any of the given special character prefixes will be assigned accordingly.
+- ## `class:__init(...any?)`
+  - Called during `class.new()` is ran, all initialization must be done here; but it's for personal preferences; like above, any properties assigned inside the function, with any of the given special character prefixes, will be assigned accordingly.
+- ## `class:__lockProperty(propName: string)`
+  - Locks the given property; prevents the property in detail from being changed. <sub>Constants are locked properties by default</sub>
+- ## `class:__unlockProperty(propName: string)`
+  - Unlocks the given; allows the property in detail from being changed <sub>Cannot unlock Constants by default</sub>
+- ## `class:__strictifyProperty(propName: string, predicate: (value: any) -> boolean)`
+  - Makes the property's value setting strict by calling `predicate` whenever `self.key = value` is done; when `predicate` returns false, it will raise an error.
+
+[/details]
+
+<div align="center"> <h4> What now? </h4> </div> <hr>
+
+Everything is now up to you once you are using it! You have complete control over how you'll manage your newly constructed class with Class as it brings new functionalities you want complete supervision on!
